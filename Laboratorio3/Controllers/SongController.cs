dd65e5431fd2a7c019c26b2e0695586a0436dc88
julian-solisaguilar <solisajulian@hotmail.com>
2022-05-03 20:13:00 -0600

    Modificacion a la pagina de Songs, ahora solo contiene una unica cancion con sus respectivos atributos sin el uso de una tabla

--- Laboratorio3/Controllers/SongController.cs
+++ Laboratorio3/Controllers/SongController.cs
@@ -10,7 +10,7 @@ namespace Laboratorio3.Controllers
     public IActionResult Index()
     {
       var songs = GetListOfSongs();
-      ViewBag.MainTitle = "List of my favorite songs";
+      ViewBag.MainTitle = "My favorite song";
       return View(songs);
     }
 
@@ -19,11 +19,6 @@ namespace Laboratorio3.Controllers
       List<SongsModel> songs = new List<SongsModel>();
       songs.Add(new SongsModel { Name = "Close To You", Author = "Dayglow", Genre = "Alternative/Indie",
         Album = "Harmony House", ReleasedDate = new DateTime (2021, 01, 12), Duration = new TimeSpan(-00,3,15) });
-      songs.Add(new SongsModel { Name = "Tiroteo - Remix", Author = "Marc Seguí, Rauw Alejandro, Pol Granch", 
-        Genre = "Alternative/Indie", Album = "Tiroteo (Remix)", ReleasedDate = new DateTime (2021, 04, 01), 
-          Duration = new TimeSpan(-00,5,21) });
-      songs.Add(new SongsModel { Name = "Nightmare", Author = "Avenged Sevenfold", Genre = "Heavy Metal",
-        Album = "Nightmare", ReleasedDate = new DateTime (2010, 07, 23), Duration = new TimeSpan(-00,6,14) });
 
       return songs;
     }
